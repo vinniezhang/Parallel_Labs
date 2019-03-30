e8acc21e5f4403e3a659c15abf468922139c8720
Vinnie <vz377@nyu.edu>
2019-03-30 17:35:50 -0400

    code changes

--- lab1/gs.c
+++ lab1/gs.c
@@ -1,8 +1,11 @@
+// Parallel Computing Lab 1
+// Vinnie Zhang 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-/*** Skeleton for Lab 1 ***/
+#include <mpi.h>
+#include <time.h>
 
 /***** Globals ******/
 float **a; /* The coefficients */
@@ -23,6 +26,7 @@ void get_input();  /* Read input from file */
 /* Function definitions: functions are ordered alphabetically ****/
 /*****************************************************************/
 
+
 /* 
    Conditions for convergence (diagonal dominance):
    1. diagonal element >= sum of all other elements of the row
@@ -38,13 +42,13 @@ void check_matrix()
   for(i = 0; i < num; i++)
   {
     sum = 0;
-    aii = fabs(a[i][i]);
+    aii = fabs(a[i][i]); // fabs() function indicates absolute value of a[i][i] matrix
     
     for(j = 0; j < num; j++)
        if( j != i)
 	 sum += fabs(a[i][j]);
        
-    if( aii < sum)
+    if( aii < sum) // matrix doesn't converge if aii < sum
     {
       printf("The matrix will not converge.\n");
       exit(1);
@@ -77,18 +81,20 @@ void get_input(char filename[])
   FILE * fp;
   int i,j;  
  
-  fp = fopen(filename, "r");
+  fp = fopen(filename, "r"); // opening file
+  
   if(!fp)
   {
     printf("Cannot open file %s\n", filename);
     exit(1);
   }
 
- fscanf(fp,"%d ",&num);
- fscanf(fp,"%f ",&err);
+ fscanf(fp,"%d ",&num); // number of unknowns
+ fscanf(fp,"%f ",&err); // relative error
 
  /* Now, time to allocate the matrices and vectors */
  a = (float**)malloc(num * sizeof(float*));
+ 
  if( !a)
   {
 	printf("Cannot allocate a!\n");
@@ -98,6 +104,7 @@ void get_input(char filename[])
  for(i = 0; i < num; i++) 
   {
     a[i] = (float *)malloc(num * sizeof(float)); 
+    
     if( !a[i])
   	{
 		printf("Cannot allocate a[%d]!\n",i);
@@ -106,6 +113,7 @@ void get_input(char filename[])
   }
  
  x = (float *) malloc(num * sizeof(float));
+
  if( !x)
   {
 	printf("Cannot allocate x!\n");
@@ -114,6 +122,7 @@ void get_input(char filename[])
 
 
  b = (float *) malloc(num * sizeof(float));
+ 
  if( !b)
   {
 	printf("Cannot allocate b!\n");
@@ -121,6 +130,7 @@ void get_input(char filename[])
   }
 
  /* Now .. Filling the blanks */ 
+
 
  /* The initial values of Xs */
  for(i = 0; i < num; i++)
@@ -135,7 +145,7 @@ void get_input(char filename[])
    fscanf(fp,"%f ",&b[i]);
  }
  
- fclose(fp); 
+ fclose(fp); // closing file
 
 }
 
@@ -146,7 +156,7 @@ void get_input(char filename[])
 int main(int argc, char *argv[])
 {
 
- int i;
+ int i, j;
  int nit = 0; /* number of iterations */
  FILE * fp;
  char output[100] ="";
@@ -166,12 +176,93 @@ int main(int argc, char *argv[])
   * This is not expected to happen for this programming assignment.
   */
  check_matrix();
+
+ float *new_x;
+ float *new_x_sum; // array of all of the x values?
+ int complete = 0; // to indicate if the rel error is <= given rel error
+ int local_complete; // indicates completion of processes
+
+// initiating arrays 
+ new_x = (float *) calloc(num, sizeof(float));
+ new_x_sum = (float *) malloc(num * sizeof(float));
+
+ int comm_sz; // number of processes in the group
+ int my_rank; // rank of current process
+ int count;
+ int first_index; // of current process
+ int last_index; // of current porcess
  
+ MPI_Init(&argc, &argv); // initializing with given arguments
+ MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); // determines size of the group (# of processes)
+ MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // determines rank of the calling process in the communicator
  
- 
+// double start, finish; // local start and finish times of processes
+// double overhead = 0; // for tracking overhead time
+// double begin, end; // used to calculate overhead time
+
+// variable declarations for later equation calculation
+ int quotient = num / comm_sz; // unknowns / processes
+ int remainder = num % comm_sz;
+
+ if (my_rank < remainder) {
+ 	count = quotient++;
+ 	first_index = my_rank*count;
+ }
+
+ else {
+ 	count = quotient;
+ 	first_index = my_rank*count + remainder;
+ }
+
+last_index = first_index + count;
+
+// do this while the rel error is not <= the given rel error
+while (complete == 0){ 
+
+	nit += 1; // iteration count increments
+	local_complete = 1; // an iteration has completed
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	// begin = MPI_Wtime(); // starting local timer
+
+	// implementation of the matrices into C code
+	for (int i = first_index; i < last_index; i++)
+	{
+		printf("Process %d now updates index %d\n", my_rank, i);
+      	new_x[i] = b[i] + a[i][i]*x[i];
+      
+      	for (j = 0; j < num; j++) {
+       		new_x[i] -= a[i][j]*x[j];
+      	}
+
+      	new_x[i] /= a[i][i];
+     
+         if ((new_x[i] - x[i]) / new_x[i] > err)
+           local_complete = 0;
+    } 
+
+    // end = MPI_Wtime(); // ending local timer
+	MPI_Barrier(MPI_COMM_WORLD); // blocks until all processes have reached this end
+	// start = MPI_Wtime(); // starting clock
+	MPI_Allreduce(new_x, new_x_sum, num, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Allreduce(&local_complete);
+	// finish = MPI_Wtime(); // end timer
+	// overhead += finish - start;
+
+	for (i = 0; i < num; i++){
+		x[i] = new_x_sum[i];
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+ 	MPI_Finalize();
+
+}
+
  /* Writing results to file */
  sprintf(output,"%d.sol",num);
  fp = fopen(output,"w");
+
  if(!fp)
  {
    printf("Cannot create the file %s\n", output);
@@ -186,5 +277,6 @@ int main(int argc, char *argv[])
  fclose(fp);
  
  exit(0);
+
 
 }
