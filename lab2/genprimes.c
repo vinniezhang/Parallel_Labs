5eea94d973573542e8179e9af9acae8cfa1b0656
Vinnie <vz377@nyu.edu>
2019-04-14 13:21:19 -0400

    adding comments

--- lab2/genprimes.c
+++ lab2/genprimes.c
@@ -1,3 +1,6 @@
+// Vinnie Zhang
+// Parallel Computing - Lab 2
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,16 +10,18 @@
 
 int main(int argc, char *argv[]) {
 
-	char *N = argv[1];
-	char *t = argv[2];
-	int num = atoi(N);
-	int thread = atoi(t);
+	char *N = argv[1]; // positive number greater than and less than or equal to 100,000 
+	char *t = argv[2]; // number of threads and is a positive int <= 100
+	int num = atoi(N); // converting from string to int
+	int thread = atoi(t); // converting from string to int
 	int stop = (num + 1) / 2;
 
+	// instantiating variables
 	int prime[num - 1];
-	double tstart = 0.0, ttaken;
+	double time_start = 0.0, time_taken;
 
-	tstart = omp_get_wtime();
+	time_start = omp_get_wtime();
+
 	#pragma omp parallel num_threads (thread)
 	{
 
@@ -40,19 +45,23 @@ int main(int argc, char *argv[]) {
 			}
 		}	
 	}
-	
-	ttaken = omp_get_wtime() - tstart;
-	printf("Time take for the main part: %f\n", ttaken);
 
+	time_taken = omp_get_wtime() - time_start; // calculating time elapsed from start
+	printf("Time take for the main part: %f\n", time_taken); // printing time elapsed
+
+	// writing to file
 	FILE *fp;
 	fp = fopen ("N.txt", "w");
-	int prevprime = 2;
+	int previous_prime = 2;
 	int index = 1;
+
 	for (int k = 0; k < num-1; k++) {
+
 		if (prime[k] != 0) {
-			fprintf(fp, "%d, %d, %d\n", index, prime[k], prime[k]-prevprime);
+
+			fprintf(fp, "%d, %d, %d\n", index, prime[k], prime[k]-previous_prime);
 			index += 1;
-			prevprime = prime[k];
+			previous_prime = prime[k];
 		}
 	}
 
