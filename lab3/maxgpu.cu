0814e7acfa3b8e1a7c3b1f076577702d1a810f65
Vinnie <vz377@nyu.edu>
2019-05-03 17:47:11 -0400

    edits

--- lab3/maxgpu.cu
+++ lab3/maxgpu.cu
@@ -58,8 +58,8 @@ int main(int argc, char *argv[])
     // Fill-up the array with random numbers from 0 to size-1 
     for(i = 0; i < array_size; i++){
        numbers[i] = rand() % array_size; 
-       printf("%d", numbers[i]);
-       printf("\n");
+       // printf("%d", numbers[i]);
+       // printf("\n");
     }
 
     // this is where the parallelizing comes in
